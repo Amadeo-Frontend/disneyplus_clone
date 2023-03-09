bf6efdffef718559012fd4fb31cbf95723159452
Amadeo Bon <amadeosph@outlook.com>
2023-03-09 03:05:44 -0300

    add:hero

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 #
-Clone da página da Disney Plus
+Clone da página da Disney +
+#
+Um clone da landing page da disney + feito com :
+HTML | CSS | JAVASCRIPT | SASS | BEM(CSS) |  GULP |
 #
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
   return gulp
@@ -8,7 +9,15 @@ function styles() {
     .pipe(gulp.dest("dist/css"));
 }
 
-exports.default = styles;
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
