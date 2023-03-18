3c5c1e496a623def60854d9f874a3323024f223a
Amadeo Bon <amadeosph@outlook.com>
2023-03-18 10:06:42 -0300

    configurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function scripts() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
 
 function styles() {
   return gulp
@@ -16,8 +24,9 @@ function images() {
     .pipe(gulp.dest("dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scripts));
 };
